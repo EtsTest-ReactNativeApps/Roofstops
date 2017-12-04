104cbbf01fe5ff6c3304ff98017f1fb32e2fc878
Ellis Mwekunda <Scareb15@gmail.com>
2017-12-04 16:12:29 -0700

    added Touchable Opacity to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, View, Image } from 'react-native';
+import { StyleSheet,View, Image,TouchableOpacity } from 'react-native';
 
 
 export default class Header extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(){
+    super()
 
 }
 render() {
   return (
 
         <View style={styles.container}>
-          <ScrollView/>
+          <TouchableOpacity onPress={this.props.homeScreen}>
           <Image
-            style ={{marginTop:20, marginBottom:10}}source={require("../../images/roofstops-logo-2.png")} />
+            style ={{marginTop:20, marginBottom:10}}source={require("../../images/roofstops-logo-2.png")}/>
+          </TouchableOpacity>
         </View>
 
 
