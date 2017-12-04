0e6d14b95be4c53fe13ca417f6db20e360df3777
Ellis Mwekunda <Scareb15@gmail.com>
2017-12-04 13:27:00 -0700

    added to review

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Review from './src/components/Review';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Review />
       </View>
     );
   }
@@ -16,7 +18,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
