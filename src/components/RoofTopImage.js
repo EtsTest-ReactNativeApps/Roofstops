1155c05a72e40bafcf80acb51483692b169379bb
Landon Shields <landonwshields@gmail.com>
2017-12-04 11:14:34 -0700

    basic structure for RoofTopImage.js

---
+++ src/components/RoofTopImage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class RoofTopImage extends React.Component {
+
+  //fetch function
+
+  render() {
+
+    return (
+      <View>
+        <Text> Fetch the image from the API </Text>
+      </View>
+    );
+  }
+}
