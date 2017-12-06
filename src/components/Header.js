acb996644b731b936b4ea4194141a605977ccef3
Paul Rodriguez <paul@prdesign.work>
2017-12-06 11:15:32 -0700

    updated logo with correct colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => (
         style ={{
           marginTop:50, marginBottom:10
         }}
-        source={require("../../images/roofstops-logo-3.png")}/>
+        source={require("../../images/roofstops-logo-5.png")}/>
     </TouchableOpacity>
   </View>
         )
